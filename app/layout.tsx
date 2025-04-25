93b93c50a235637b29024b685f848e6512d6eaab
Pete Royle <peter.royle@oneadvanced.com>
2025-04-25 14:02:17 +0100

    Stuff
    Stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import './globals.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  title: 'Glyph - The OneAdvanced File Processing Agent',
+  description: 'A one stop shop for file processing.',
 };
 
 export const viewport = {
