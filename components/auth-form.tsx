d47861201bde0d5d355509a23e3292d864718625
Pete Royle <peter.royle@oneadvanced.com>
2025-05-02 13:42:30 +0100

    logo and better sign in page

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -1,7 +1,13 @@
 import Form from 'next/form';
-
 import { Input } from './ui/input';
 import { Label } from './ui/label';
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from './ui/select'; // Import the necessary components
 
 export function AuthForm({
   action,
@@ -16,6 +22,8 @@ export function AuthForm({
 }) {
   return (
     <Form action={action} className="flex flex-col gap-4 px-4 sm:px-16">
+
+
       <div className="flex flex-col gap-2">
         <Label
           htmlFor="email"
@@ -23,7 +31,6 @@ export function AuthForm({
         >
           Email Address
         </Label>
-
         <Input
           id="email"
           name="email"
@@ -44,7 +51,6 @@ export function AuthForm({
         >
           Password
         </Label>
-
         <Input
           id="password"
           name="password"
@@ -52,6 +58,47 @@ export function AuthForm({
           type="password"
           required
         />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <Label
+          htmlFor="mfacode"
+          className="text-zinc-600 font-normal dark:text-zinc-400"
+        >
+          MFA Code
+        </Label>
+        <Input
+          id="mfacode"
+          name="mfacode"
+          className="bg-muted text-md md:text-sm"
+          type="password" // Changed the type to "password"
+          required
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <Label
+          htmlFor="verticalslice"
+          className="text-zinc-600 font-normal dark:text-zinc-400"
+        >
+          Persona Profile
+        </Label>
+        <Select name="verticalslice">
+          <SelectTrigger className="bg-muted text-md md:text-sm">
+            <SelectValue placeholder="Please Select ..." />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="Care">Care</SelectItem>
+            <SelectItem value="Commercial">Commercial</SelectItem>
+            <SelectItem value="Education">Education</SelectItem>
+            <SelectItem value="Government">Government</SelectItem>
+            <SelectItem value="Health">Health</SelectItem>
+            <SelectItem value="Housing">Housing</SelectItem>
+            <SelectItem value="Logistics">Logistics</SelectItem>
+            <SelectItem value="Legal">Legal</SelectItem>
+            <SelectItem value="Retail">Retail</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
 
       {children}
